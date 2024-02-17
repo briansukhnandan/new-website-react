81c2f901493543c12925304cbbb7362b2468460f
Brian Sukhnandan <briansukhnandan@gmail.com>
2024-02-16 22:27:56 -0500

    editsa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,44 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <OpeningHtmlTag />
       <Header />
+      <Body />
+      <ClosingHtmlTag/>
     </main>
   );
 }
 
-const Header = () => {
+const Header = () => (
+  <div style={{ textAlign: "center", paddingTop: "10px" }}>
+    <pre>
+      __________       .__                <br/>
+      \______   \______|__|____    ____   <br/>
+      |    |  _/\_  __ \  \__  \  /    \  <br/>
+      |    |   \ |  | \/  |/ __ \|   |  \ <br/>
+      |________/ |__|  |__(____  /___|  / <br/>
+      ________________________ \/     \/  <br/>
+    </pre>
+  </div>
+);
+
+
+const Body = () => {
   return (
-    <div>
-      <pre>
-        __________       .__                <br/>
-        \______   \______|__|____    ____   <br/>
-        |    |  _/\_  __ \  \__  \  /    \  <br/>
-        |    |   \ |  | \/  |/ __ \|   |  \ <br/>
-        |________/ |__|  |__(____  /___|  / <br/>
-        ________________________ \/     \/  <br/>
-      </pre>
+    <div style={{ textAlign: "center" }}>
+      {"TODO"}
     </div>
   )
 }
+
+const OpeningHtmlTag = () => (
+  <div style={{ textAlign: "right" }}>
+    {"<Brian>"}
+  </div>
+);
+
+const ClosingHtmlTag = () => (
+  <div style={{ textAlign: "left" }}>
+    {"</Brian>"}
+  </div>
+);
